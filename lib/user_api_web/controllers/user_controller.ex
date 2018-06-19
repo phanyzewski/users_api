83bb3e184a0389e59dbf0116c2247cebb7a44050
Paul Hanyzewski <hanyzewski@gmail.com>
2018-06-19 17:51:55 -0600

    add session and save authentication status

--- lib/user_api_web/controllers/user_controller.ex
+++ lib/user_api_web/controllers/user_controller.ex
@@ -44,11 +44,13 @@ defmodule UserApiWeb.UserController do
     case UserApi.Auth.authenticate_user(email, password) do
       {:ok, user} ->
         conn
+        |> put_session(:current_user_id, user.id)
         |> put_status(:ok)
         |> render(UserApiWeb.UserView, "sign_in.json", user: user)
 
       {:error, message} ->
         conn
+        |> delete_session(:current_user_id)
         |> put_status(:unauthorized)
         |> render(UserApiWeb.ErrorView, "401.json", message: message)
     end
--- lib/user_api_web/router.ex
+++ lib/user_api_web/router.ex
@@ -3,11 +3,33 @@ defmodule UserApiWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug :fetch_session
+  end
+
+  pipeline :api_auth do
+    plug :ensure_authentication
   end
 
   scope "/api", UserApiWeb do
-    pipe_through :api
-    resources "/users", UserController, except: [:new, :edit]
+    pipe_through(:api)
     post "/users/sign_in", UserController, :sign_in
+  end
+
+  scope "/api", UserApiWeb do
+    pipe_through([:api, :api_auth])
+    resources "/users", UserController, except: [:new, :edit]
+  end
+
+  defp ensure_authentication(conn, _opts) do
+    current_user_id = get_session(conn, :current_user_id)
+
+    if current_user_id do
+      conn
+    else
+      conn
+      |> put_status(:unauthorized)
+      |> render(UserApiWeb.ErrorView, "401.json", message: "Unauthenticated user")
+      |> halt()
+    end
   end
 end
